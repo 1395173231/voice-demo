3f05f35b0b9b3f8998f5600a45b2d31d1044d427
AhhhLiu <s1395173231@gmail.com>
2024-05-25 15:01:16 +0800

    first

---
+++ app/api/get-voice-token/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from 'next/server';
+import cors from '../../../lib/cors';
+
+export async function handle(req: NextRequest) {
+  let authKey = process.env.VOICE_API_KEY;
+  let baseUrl = process.env.VOICE_BASE_URL;
+  if (!authKey || !baseUrl) {
+    return NextResponse.json({ status: 401, statusText: 'Not authorized, Need config env' });
+  }
+  const fetchOptions: RequestInit = {
+    headers: {
+      Authorization: 'Bearer ' + authKey,
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-store',
+    method: 'POST',
+    // @ts-ignore
+    duplex: 'half',
+    body: req.body,
+  };
+  let resp = await fetch(`${baseUrl}/v1/voice/get_token`, fetchOptions);
+  let result = await resp.json();
+  result.url = process.env.NEXT_PUBLIC_LIVEKIT_URL;
+  return cors(
+    req, NextResponse.json(result, {
+      headers: {
+        'content-type': 'application/json; charset=utf-8',
+      },
+    }));
+}
+
+export const POST = handle;
+export const GET = handle;
+
+export async function OPTIONS(request: Request) {
+  return cors(
+    request,
+    new Response(null, {
+      status: 200,
+    }),
+  );
+}
+
+export const runtime = 'edge';
